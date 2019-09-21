18cf7b167c9f71f53c312f680bc8e5023795dc6b
Jeremy Campbell <JCAMPBELL2@harding.edu>
2019-09-21 13:55:46 -0500

    Recursively populate the current directory tree view.

--- PhotoEdit/MainForm.cs
+++ PhotoEdit/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,34 @@ namespace PhotoEdit
 {
     public partial class MainForm : Form
     {
+        private DirectoryInfo rootDir; 
+
         public MainForm()
         {
             InitializeComponent();
+            InitializeTreeView();
+        }
+
+        private void InitializeTreeView()
+        {
+            // https://stackoverflow.com/a/116061
+            string picturesPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            rootDir = new DirectoryInfo(picturesPath);
+
+            PopulateDirectoryTreeView(rootDir, currentDirectoryTreeView.Nodes);
+            currentDirectoryTreeView.ExpandAll();
+        }
+
+        private void PopulateDirectoryTreeView(DirectoryInfo rootDir, TreeNodeCollection nodes)
+        {
+            string dirName = rootDir.Name;
+            nodes.Add(dirName, dirName);
+            nodes[dirName].Tag = rootDir.FullName;
+
+            foreach (DirectoryInfo subDir in rootDir.GetDirectories())
+            {
+                PopulateDirectoryTreeView(subDir, nodes[dirName].Nodes);
+            }
         }
     }
 }
