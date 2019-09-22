b58582cf43f230477a88af096d4163e42f2349aa
Jeremy Campbell <JCAMPBELL2@harding.edu>
2019-09-22 12:33:02 -0500

    Adjust MainForm size.

--- PhotoEdit/MainForm.cs
+++ PhotoEdit/MainForm.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
