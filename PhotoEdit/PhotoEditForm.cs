156f0ddbc09b577923d7a894b5a4bee06bc996d4
caleb_rudder <caleb_rudder@yahoo.com>
2019-09-21 21:26:20 -0500

    update controls

--- PhotoEdit/PhotoEditForm.cs
+++ PhotoEdit/PhotoEditForm.cs
@@ -12,9 +12,11 @@ namespace PhotoEdit
 {
     public partial class PhotoEditForm : Form
     {
-        public PhotoEditForm()
+        public PhotoEditForm(String filePath)
         {
+            var selectedPhoto = new Bitmap(Image.FromFile(filePath));
             InitializeComponent();
+            imageView.BackgroundImage = selectedPhoto;
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -22,14 +24,28 @@ namespace PhotoEdit
 
         }
 
-        private void BrightnessBar_Scroll(object sender, EventArgs e)
+        async private void BrightnessBar_Scroll(object sender, EventArgs e)
         {
-
+            await ChangeImageBrightness(brightnessBar.Value);
         }
 
         private void InvertButton_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        async Task ChangeImageBrightness(int value)
+        {
+            await Task.Run(() =>
+            {
+
+
+            });
         }
     }
 }
